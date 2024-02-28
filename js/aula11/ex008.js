23a73fb308ab9b57f37a749281eb6b3152b8b415
Ana Luiza Dias <126631257+AnnaDays@users.noreply.github.com>
2024-02-28 16:01:13 -0300

    exercicio - hora do dia
    protótipo do exercicio, não tive tempo de terminá-lo

---
+++ js/aula11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 34
+console.log(`A velocidade do seu carro é ${velocidade}`)
+if (velocidade > 60) { // Condição simples
+    console.log(`Você passou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
