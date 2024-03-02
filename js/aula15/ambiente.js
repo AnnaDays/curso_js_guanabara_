387d3c25c4e902baa92b78eac8a8290fd23399c1
Ana Luiza Dias <126631257+AnnaDays@users.noreply.github.com>
2024-03-01 22:56:53 -0300

    Create ambiente.js

---
+++ js/aula15/ambiente.js
@@ -0,0 +1,10 @@
+let num = [5, 8, 2, 9, 3]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+for(let pos=0; pos<num.length;pos++){
+    console.log(num[pos])
+}
