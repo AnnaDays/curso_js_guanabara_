7762b007f3598efc997d31c458fd125f213e2c5e
Ana Luiza Dias <126631257+AnnaDays@users.noreply.github.com>
2024-03-04 12:19:47 -0300

    funções

--- js/aula15/ambiente.js
+++ js/aula15/ambiente.js
@@ -1,10 +1,27 @@
 let num = [5, 8, 2, 9, 3]
-num.push(1)
-num.sort()
-console.log(num)
-console.log(`O vetor tem ${num.length} posições`)
-console.log(`O primeiro valor do vetor é ${num[0]}`)
+// num.push(1)
+// num.sort()
+// console.log(num)
+// console.log(`O vetor tem ${num.length} posições`)
+// console.log(`O primeiro valor do vetor é ${num[0]}`)
 
-for(let pos=0; pos<num.length;pos++){
-    console.log(num[pos])
+// for(let pos=0; pos<num.length;pos++){
+//     console.log(num[pos])
+// }
+
+let valores = [8, 1, 7, 4, 2, 9]
+
+// for(let pos=0; pos < valores.length; pos++){
+//     console.log(`A posição ${pos} tem o valor ${valores[pos]}`) //é chamado de percurso pra exibição de vetores em JS
+// }
+
+// for(let pos in valores){ //PARA cada posição DENTRO de valores....
+//     console.log(`A posição ${pos} tem o valor ${valores[pos]}`) //Versão simplificada
+// }
+
+let pos = num.indexOf(8)
+if(pos == -1){
+    console.log('O valor não foi encontrado')
+} else{
+    console.log(`O valor 8 está na posição ${pos}`)
 }
---
+++ js/aula16/funcao02.js
@@ -0,0 +1,4 @@
+function soma(n1=0, n2=0){ //se por acaso não passar nenhumm numero no parâmetro, considere o 0
+    return n1 + n2
+}
+console.log(soma(7))
